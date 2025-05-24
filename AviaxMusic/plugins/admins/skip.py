f910c92958d169aa728386d7c681f27798eb6c5d
‹ 𝚲 𝚼 𝐈 › <164884298+Notookk@users.noreply.github.com>
2025-05-24 09:00:49 +0530

    Update skip.py

--- AviaxMusic/plugins/admins/skip.py
+++ AviaxMusic/plugins/admins/skip.py
@@ -115,7 +115,7 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await message.reply_text(_["call_6"])
         button = stream_markup(_, chat_id)
-        img = await gen_thumb(videoid, user))
+        img = await gen_thumb(videoid, user)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
